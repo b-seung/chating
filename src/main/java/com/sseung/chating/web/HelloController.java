a83f9e5ab29aeeaca6e244de3805693f34c3a90b
user <kamo0915@gmail.com>
2023-04-19 17:33:39 +0900

    230419

--- src/main/java/com/sseung/chating/web/HelloController.java
+++ src/main/java/com/sseung/chating/web/HelloController.java
@@ -1,7 +1,12 @@
 package com.sseung.chating.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
