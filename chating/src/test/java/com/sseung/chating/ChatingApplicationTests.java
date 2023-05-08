8f6a00a6a07b8ffa7c897f3b930ac5125c698770
user <baesh@planit.jp>
2023-05-09 02:32:13 +0900

    230508

---
+++ chating/src/test/java/com/sseung/chating/ChatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sseung.chating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
